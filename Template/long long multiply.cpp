3685058cecdcc2a5b303d5ad46c42657ba0418d0
ZexinLee <lizexin0903@gmail.com>
2019-09-02 16:50:16 +0800

    :boom: add long long multiply template

---
+++ Template/long long multiply.cpp
@@ -0,0 +1,13 @@
+    const long long MOD = 1000000007L;
+
+    long long mul(long long a, long long b) {
+        long long ans = 0, step = a % MOD;
+        while (b) {
+            if (b & 1L) ans += step;
+            if (ans >= MOD) ans %= MOD;
+            step <<= 1L;
+            if (step >= MOD) step %= MOD;
+            b >>= 1L;
+        }
+        return ans % MOD;
+    }
