5898028669c09baca255c2de97f4d40c6b816c15
ZexinLi0w0 <lizexin0903@gmail.com>
2021-02-01 01:41:45 +0000

    add install.sh for remote server

---
+++ install.sh
@@ -0,0 +1,18 @@
+     apt update -y
+     apt upgrade -y
+     apt install vim htop tmux git -y
+     apt install zsh -y
+     wget https://github.com/ehang-io/nps/releases/download/v0.26.9/linux_386_server.tar.gz
+     tar zvxf linux_386_server.tar.gz
+     mkdir nps
+     mv linux_386_server.tar.gz nps
+     cd nps
+     tar zvxf linux_386_server.tar.gz
+     ./nps install
+     nps start
+     sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+     wget https://github.com/ZexinLi0w0/LeetCode/blob/master/.zshrc
+     cp .zshrc /root/.zshrc 
+     zsh
